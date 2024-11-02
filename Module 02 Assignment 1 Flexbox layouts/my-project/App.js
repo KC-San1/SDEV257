b62abcbfc7f9e64b45cdf7f03ce8ef7daa9bb5f7
KC-San1 <77395031+KC-San1@users.noreply.github.com>
2024-11-02 18:40:14 -0400

    2

--- Module 02 Assignment 1 Flexbox layouts/my-project/App.js
+++ Module 02 Assignment 1 Flexbox layouts/my-project/App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StatusBar } from "react-native";
+import styles from "./styles";
+import Row from "./Row";
+import Column from "./Column";
+import Box from "./Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-       <StatusBar hidden={false} />
+      <StatusBar hidden={false} />
       <Row>
         <Column>
           <Box>#1</Box>
@@ -38,48 +42,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "ghostwhite",
-    alignItems: "center",
-    justifyContent: "space-around",
-    ...Platform.select({
-      ios: { paddingTop: 40 },
-      android: { paddingTop: StatusBar.currentHeight },
-    }),
-  },
-
-  box: {
-    height: 100,
-    width: 100,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderColor: "darkslategray",
-    backgroundColor: "lightgray",
-  },
-
-  boxText: {
-    color: "darkslategray",
-    fontWeight: "bold",
-  },
-
-  row: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignSelf: "stretch",
-  },
-
-  column: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-around",
-    alignSelf: "stretch",
-  },
-});
