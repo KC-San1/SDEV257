9ec5e8ea7a13ea07f3231743a6f38386438ef040
KC-San1 <77395031+KC-San1@users.noreply.github.com>
2024-11-11 00:20:34 -0500

    dafh

--- Module 02 Assignment Starwars - complete/my-project/Home.js
+++ Module 02 Assignment Starwars - complete/my-project/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import ListContainer from "./ListContainer";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Planets Content</Text>
+      <ListContainer />
     </View>
   );
 }
--- Module 02 Assignment Starwars - complete/my-project/Settings.js
+++ Module 02 Assignment Starwars - complete/my-project/Settings.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import ListContainer from "./ListContainer";
 
 export default function Settings({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Films Content</Text>
+      <ListContainer />
     </View>
   );
 }
